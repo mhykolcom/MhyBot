0169265acc19f210dde3f67d56826b0dd54ebaea
Mhykol <mhykol@gmail.com>
2018-07-22 20:00:40 -0500

    Setting up environment

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { ApiRequester } from "twitch-wrapper-ts";
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const twitchApi = new TwitchApi(clientId, oauth);
