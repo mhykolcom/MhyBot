f1405dba39d09285e310b3afa570c60b7bf7eb98
Mhykol <mhykol@gmail.com>
2018-07-23 20:01:14 -0500

    RESTART!

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { ApiRequester } from "twitch-wrapper-ts";
-
-
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const twitchApi = new TwitchApi(clientId, oauth);
--- src/mongo/index.ts
+++
@@ -1,6 +0,0 @@
-export class Mongo {
-
-    public constructor() {
-
-    }
-}
