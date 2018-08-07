8701322285e2de35e7576ff5e716831ba85af18a
Mhykol <mhykol@gmail.com>
2018-08-07 10:36:19 -0500

    Add logging and ignore .log files

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-  require('winston')
-  const 
+winston = require('winston')
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    transports: [
+      //
+      // - Write to all logs with level `info` and below to `combined.log` 
+      // - Write all logs error (and below) to `error.log`.
+      //
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+});
+const 
     fs = require('fs'),
     Discord = require('discord.js'),
     client = new Discord.Client(),
