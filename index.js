c0a62e6bc32dd54e6869a11fa336dcdeacee8e5d
Mhykol <mhykol@gmail.com>
2019-09-02 23:02:25 -0500

    Logging change

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ const alignedWithColorsAndTime = winston.format.combine(
         level: 'info',
         format: winston.format.json(),
         transports: [
+            new winston.transports.Console({ format: winston.format.simple() }),
             //new winston.transports.Console({ format: alignedWithColorsAndTime }),
-            new winston.transports.Console({ level: 'info' }),
             new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
         ]
     });
